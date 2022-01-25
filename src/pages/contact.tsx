8311d7434c38f5ccd8067fb69f2b2ee63ddf6510
Milos Obrovac <obrovac.milos@gmail.com>
2022-01-26 00:02:57 +0100

    [ADD] contact icons

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,6 +4,10 @@ import t from "@translate";
 import Seo from "../components/seo";
 import { MainBottomPadding } from "../layout/layout";
 import { Container, styled } from "@mui/material";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
 import Title from "../components/title";
 import Map from "../components/pages/contact/map";
 import SideContainer from "../components/sideContainer";
@@ -36,18 +40,63 @@ const MapContainer = styled("div")(({ theme }) => ({
   marginBottom: `-${MainBottomPadding}`,
 }));
 
+const contactGroupClass = "contact-group";
+const ContactGroup = styled("div")(({ theme }) => ({
+  // borderTop: `0.25rem double black`,
+  // paddingTop: "1rem",
+  marginTop: theme.spacing(3),
+
+  [`& .${contactGroupClass}`]: {
+    display: "flex",
+    "& ul": {
+      padding: 0,
+      marginLeft: theme.spacing(3),
+      listStyle: "none",
+    },
+    "& li": {
+      padding: 0,
+      margin: 0,
+      listStyle: "none",
+    },
+  },
+}));
+
 const ContactContent = () => (
   <>
     <Title>{t("pages.contact.title")}</Title>
     <p>{t("pages.contact.text")}</p>
-    <ul>
-      <li>{t("global.contact.contactNumber")}</li>
-      <li>{t("global.contact.street")}</li>
-      <li>{t("global.contact.zipCity")}</li>
-      <li>{t("global.contact.country")}</li>
-      <li>{t("global.contact.contactPerson")}</li>
-      <li>{t("global.contact.contactEmail")}</li>
-    </ul>
+
+    <ContactGroup>
+      <div className={contactGroupClass}>
+        <LocalPhoneIcon />
+        <ul>
+          <li>{t("global.contact.contactNumber")}</li>
+        </ul>
+      </div>
+
+      <div className={contactGroupClass}>
+        <AlternateEmailIcon />
+        <ul>
+          <li>{t("global.contact.contactEmail")}</li>
+        </ul>
+      </div>
+
+      <div className={contactGroupClass}>
+        <PersonIcon />
+        <ul>
+          <li>{t("global.contact.contactPerson")}</li>
+        </ul>
+      </div>
+
+      <div className={contactGroupClass}>
+        <HomeIcon />
+        <ul>
+          <li>{t("global.contact.street")}</li>
+          <li>{t("global.contact.zipCity")}</li>
+          <li>{t("global.contact.country")}</li>
+        </ul>
+      </div>
+    </ContactGroup>
   </>
 );
 
